9970fd2813b043d72ecaaea117a8a98627b6705f
thundererr <surajprasadiniiita@gmail.com>
2020-04-28 12:55:31 +0530

    express middleware

--- Assignment/Node/Website/app.js
+++ Assignment/Node/Website/app.js
@@ -18,9 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Assignment/Node/Website/routes/index.js
+++ Assignment/Node/Website/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var path = require("path"); 
+var app = express();
 var router = express.Router();
 
+
+//middleware use
+const logger = function (req,res,next){
+	console.log("logged at : " + new Date());
+}
+
+//router.use('/', logger);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/',function(req,res) {
+//   res.sendFile("index1.html");
+//   //__dirname : It will resolve to your project folder.
+// });
+
+
 
 module.exports = router;
